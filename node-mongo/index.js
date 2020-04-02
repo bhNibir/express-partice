0ef6162be547f69c065f0e488f0e06e8733632b3
nibir <biplob.asanibir@gmail.com>
2020-04-02 07:36:21 +0600

    sent database

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -3,6 +3,12 @@ const app = express()
 const port = 3000
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient;
+
+const dbUser = "dbUser"
+const dbPass = "8pIFwdHON3Olrb1z"
+const uri = "mongodb+srv://dbUser:8pIFwdHON3Olrb1z@cluster0-gyjtn.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
 
 app.use(cors())
 app.use(bodyParser.json())
@@ -10,14 +16,27 @@ app.use(bodyParser.json())
 const names = ["Babu", "Nibir", "Biplob", "Hasan"]
 
 
+
+
+
+
+
 app.get('/user/:id', (req, res) => {
     const id = req.params.id
     res.send({id , name:names[id]})
 })
 
-app.post('/adduser', (req,res) => {
-    console.log(req.body);
-    res.send(req.body)
+app.post('/addProduct', (req,res) => {
+    const product = req.body
+    console.log(product)
+    client.connect(err => {
+        const collection = client.db("commerce").collection("products");
+          collection.insertOne(product,(err, res) => {
+              console.log("add data", res);
+          })
+          
+        client.close();
+      });
     
 })
 app.listen(port, () => {console.log(`App listening ${port}`)})
