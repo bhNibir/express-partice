edd6061a8f1881eb9fd55821578f65da43fedecb
nibir <biplob.asanibir@gmail.com>
2020-04-02 09:40:40 +0600

    add env

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -1,21 +1,37 @@
 const express = require('express')
+require('dotenv').config()
 const app = express()
 const port = 3000
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient;
 
-const dbUser = "dbUser"
-const dbPass = "8pIFwdHON3Olrb1z"
-const uri = "mongodb+srv://dbUser:8pIFwdHON3Olrb1z@cluster0-gyjtn.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
+const uri = process.env.DB_PATH
+let client = new MongoClient(uri, { useNewUrlParser: true });
 
 app.use(cors())
 app.use(bodyParser.json())
 
 const names = ["Babu", "Nibir", "Biplob", "Hasan"]
 
-
+app.get('/product' , (req,res) => {
+    client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+        const collection = client.db("commerce").collection("products");
+          collection.find({name:'Phone'}).limit(5).toArray((err, documents) => {
+             if(err){
+                 console.log(err);
+                 res.status(500).send({message:err})
+                 
+             }
+             else{
+                 res.send(documents)
+             }
+          })
+          
+        client.close();
+      });
+})
 
 
 
@@ -28,15 +44,24 @@ app.get('/user/:id', (req, res) => {
 
 app.post('/addProduct', (req,res) => {
     const product = req.body
-    console.log(product)
+
+    client = new MongoClient(uri, { useNewUrlParser: true });
     client.connect(err => {
         const collection = client.db("commerce").collection("products");
-          collection.insertOne(product,(err, res) => {
-              console.log("add data", res);
+          collection.insertOne(product,(err, result) => {
+             if(err){
+                 console.log(err);
+                 res.status(500).send({message:err})
+                 
+             }
+             else{
+                 res.send(result.ops[0])
+             }
           })
           
         client.close();
       });
     
 })
+
 app.listen(port, () => {console.log(`App listening ${port}`)})
